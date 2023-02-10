45598f19263dbb5ac4b8d1be9e69c910dcc02a51
Ella Svete <ella.svete@gmail.com>
2023-02-09 17:20:08 -0800

    positioned and styled button

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -15,7 +15,7 @@ const SearchExercises = () => {
               border: 'none', 
               borderRadius: '4px'
             }, 
-            width: { lg: '1170px', xs: '350px'}, 
+            width: { lg: '800px', xs: '350px'}, 
             backgroundColor: '#fff', borderRadius: '40px'
           }}
           height="76px"
@@ -24,6 +24,19 @@ const SearchExercises = () => {
           placeholder="Search Exercises"
           type="text"
         />
+        <Button className="search-btn"
+          sx={{
+            bgcolor: '#FF2625', 
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px'},
+            fontSize: { lg: '20px', xs: '14px'},
+            height: '56px',
+            position: 'absolute'
+          }}
+        >
+          Search
+        </Button>
       </Box>
 
 
